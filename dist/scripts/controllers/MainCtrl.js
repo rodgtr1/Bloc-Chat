e5c57cd2cf7926d97abf40f0a50c1132c849319c
Travis Rodgers <rodgtr1@gmail.com>
2017-02-06 16:06:26 -0500

    messages now showing by roomId

--- dist/scripts/controllers/MainCtrl.js
+++ dist/scripts/controllers/MainCtrl.js
@@ -1,15 +1,33 @@
 (function() {
-  function MainCtrl(Room, $uibModal) {
+  function MainCtrl(Message, Room, $uibModal) {
     /* getRooms is a function created in Room.js and .all is the object calling roomsArray. We inject the Room service into this controller */
     this.chatrooms = Room.getRooms().all;
+    
+    
+    
+    // load without a room selected. Will be used to hold current room.
+    this.currentRoom = null;
+    
+    // states whether room name is shown or not
+    this.currentRoomShowing = false;
+    
+    this.messages = null;
+    
+    //sets the current rooom to the one clicked
+    this.setCurrentChatRoom = function(clickedRoom) {
+      this.currentRoom = clickedRoom;
+      this.messages = Message.getByRoomId(this.currentRoom.$id);
+    };
+    
   
-  this.openModal = function() {
+    //method for Main Controller to open modal 
+    this.openModal = function() {
     
     var modalInstance = $uibModal.open({ 
       //template url correspond to modal to be opened
       templateUrl: '/templates/modal.html',
 
-      //controller for modal
+      //controller for modal. Opted to include here instead of separate controller (i.e. ModalCtrl.js)
       controller: function($scope, $uibModalInstance) {
         $scope.newRoom = {$value: ''};
 
@@ -31,8 +49,10 @@
 
     };
   };
+  
+  
 
   angular
     .module('blocChat')
-    .controller('MainCtrl', ['Room', '$uibModal', MainCtrl]);
+    .controller('MainCtrl', ['Message', 'Room', '$uibModal', MainCtrl]);
 })();
