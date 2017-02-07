97b8869ec8d2efe39ace67afbaeb155707670243
Travis Rodgers <rodgtr1@gmail.com>
2017-02-06 21:57:58 -0500

    set username via run method and modal complete

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -10,11 +10,7 @@
         // Filter the messages by their room ID.
         
         return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
-      };
-      
-      send: function(newMessage) {
-      
-    }
+      }
     };
   }
 
